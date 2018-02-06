40ab5a22249be893af7f99d12636d126e7780fe0
GeorgeFreeman <thenewsit@gmail.com>
2018-02-06 22:39:57 +0500

    Natcoin: fix consensus

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -102,10 +102,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000ba50a60f8b56c7fe0");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000002900290");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x29c8c00e1a5f446a6364a29633d3f1ee16428d87c8d3851a1c570be8170b04c2"); //1259849
+        consensus.defaultAssumeValid = uint256S("0x2d0b947825a76c76b62acd784e523d65d800b1ed57d2671d7676838ce01a7f3e"); //40
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
